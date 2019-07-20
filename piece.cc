c4dcee52ada03f360fbbb48293554063290a56da
Ada-Armstrong <christopherada.a@gmail.com>
2019-07-20 11:28:47 -0400

    partial implementation of piece

---
+++ piece.cc
@@ -0,0 +1,101 @@
+#include "piece.h"
+
+Bitboard Piece::get_pos() const {
+	return pos;
+}
+
+Bitboard Piece::get_sudo_legal_moves() const
+{
+	return sudo_legal_moves;
+}
+
+Bitboard Piece::get_one_deep_moves() const
+{
+	return one_deep_moves;
+}
+
+Bitboard Piece::get_legal_moves() const
+{
+	return legal_moves;
+}
+
+void Piece::set_pos(Bitboard new_pos) 
+{
+	pos = new_pos;
+}
+
+static Bitboard sudo_legal_pawn(Piece &p, Bitboard friends, Bitboard enemies,
+		bool pawn_move)
+{
+	Bitboard slm{0};
+	Bitboard tmp{0};
+	const Bitboard empty{0};
+	const Bitboard filled = friends | enemies;
+
+	if (team == Colour::WHITE) {
+		// diagonal attacks, left and right respectively
+		slm |= p.pos << (COL_SHIFT - 1);
+		slm |= p.pos << (COL_SHIFT + 1);
+
+		if (pawn_move) {
+			// single move
+			tmp |= (p.pos << COL_SHIFT) & ~filled;
+			// double move
+			tmp |= (tmp && p.pos & WHITE_DOUBLE_ROW) ? 
+				(p.pos << 2 * COL_SHIFT) & ~filled : empty;
+			slm |= tmp;
+		}	
+	} else {
+		slm |= p.pos >> (COL_SHIFT - 1);
+		slm |= p.pos >> (COL_SHIFT + 1);
+		if (pawn_move) {
+			// single move
+			tmp |= (p.pos >> COL_SHIFT) & ~filled;
+			// double move
+			tmp |= (tmp && p.pos & BLACK_DOUBLE_ROW) ? 
+				(p.pos >> 2 * COL_SHIFT) & ~filled : empty;
+			slm |= tmp;
+		}
+	}
+
+	return slm & VALID_BOARD;
+}
+
+static Bitboard sudo_legal_knight(Piece &p)
+{
+	Bitboard slm{0};
+
+	slm |= p.pos << COL_SHIFT + 2;
+	slm |= p.pos << (2 * COL_SHIFT)  + 1;
+	slm |= p.pos << COL_SHIFT - 2;
+	slm |= p.pos << (2 * COL_SHIFT) - 1;
+	slm |= p.pos >> COL_SHIFT + 2;
+	slm |= p.pos >> (2 * COL_SHIFT) + 1;
+	slm |= p.pos >> COLSHIFT - 2;
+	slm |= p.pos >> (2 * COL_SHIFT) - 1;
+
+	return slm & VALID_BOARD;
+}
+
+void Piece::calc_sudo_legal_moves(Bitboard friends, Bitboard enemies,
+		bool pawn_move)
+{
+	switch (type) {
+	case Type::PAWN:
+		sudo_legal_moves = sudo_legal_pawn(*this, friends, enemies, pawn_move);
+		break;
+	case Type::ROOK:
+		break;
+	case Type::KNIGHT:
+		sudo_legal_moves = sudo_legal_knight(*this);
+		break;
+	case Type::BISHOP:
+		break;
+	case Type::QUEEN:
+		break;
+	case Type::KING:
+		break;
+	default:
+		// exception
+	}
+}
---
+++ piece.h
@@ -0,0 +1,24 @@
+#ifndef _PIECE_H_
+#define _PIECE_H_
+
+#include "bitboard.h"
+#include "type.h"
+
+class Piece {
+	Bitboard pos; // a single 1 at the position where the piece is
+	Bitboard sudo_legal_moves; // the moves the piece can make according to it's type, includes moving onto a friendly piece
+	Bitboard one_deep_moves; // the moves the piece can make according to it's type, skipping over the first enemy it encounters
+	Bitboard legal_moves; // the moves the piece can make according to it's type, and not putting the king in check
+	Type type;
+	Colour team;
+	
+	public:
+	Bitboard get_pos() const;
+	Bitboard get_sudo_legal_moves(Bitboard enemies, bool pawn_move) const;
+	Bitboard get_one_deep_moves() const;
+	Bitboard get_legal_moves() const;
+	void set_pos(Bitboard new_pos);
+	void calc_sudo_legal_moves();
+	void calc_one_deep_moves();
+	void calc_legal_moves();
+};
