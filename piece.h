490a6d826a36c1ac0316e5a7b2aec39039afd98b
Ada-Armstrong <christopherada.a@gmail.com>
2019-07-21 10:10:37 -0400

    fixed declarations

--- piece.h
+++ piece.h
@@ -5,21 +5,30 @@
 #include "type.h"
 
 class Piece {
-	Bitboard pos; // a single 1 at the position where the piece is
-	Bitboard sudo_legal_moves; // the moves the piece can make according to it's type, includes moving onto a friendly piece
-	Bitboard one_deep_moves; // the moves the piece can make according to it's type, skipping over the first enemy it encounters
-	Bitboard legal_moves; // the moves the piece can make according to it's type, and not putting the king in check
+	// a single 1 at the position where the piece is
+	Bitboard pos;
+	// the moves the piece can make according to it's type,
+	// includes moving onto a friendly piece
+	Bitboard sudo_legal_moves;
+	// the moves the piece can make according to it's type,
+	// skipping over the first enemy it encounters
+	Bitboard one_deep_moves;
+	// the moves the piece can make according to it's type,
+	// and not putting the king in check
+	Bitboard legal_moves;
 	Type type;
 	Colour team;
 	
 	public:
 	Bitboard get_pos() const;
-	Bitboard get_sudo_legal_moves(Bitboard enemies, bool pawn_move) const;
+	Bitboard get_sudo_legal_moves() const;
 	Bitboard get_one_deep_moves() const;
 	Bitboard get_legal_moves() const;
 	void set_pos(Bitboard new_pos);
-	void calc_sudo_legal_moves();
-	void calc_one_deep_moves();
+	void calc_sudo_legal_moves(const Bitboard friends,
+			const Bitboard enemies, const bool pawn_move);
+	void calc_one_deep_moves(const Bitboard friends,
+			const Bitboard enemies);
 	void calc_legal_moves();
 };
 
