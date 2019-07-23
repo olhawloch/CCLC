08c0c39d9dcd5c3e9103f188835dca7c4dc9a4ad
Ada-Armstrong <christopherada.a@gmail.com>
2019-07-23 12:53:07 -0400

    comments

--- team.h
+++ team.h
@@ -11,25 +11,36 @@ class Team {
 	Colour colour;
 	
 	public:
+	// caclulate once per broadstate
 	bool check(Bitboard enemies_atk) const;
+	// caclulate once per broadstate
 	Piece *checking_piece(const Bitboard enemy_king_pos) const;
+	// caclulate once per boardstate
 	bool is_double_check(const Bitboard enemy_king_pos) const;
+	// caclulate once per boardstate
 	Bitboard pos_pieces() const;
 	// assume we have up-to-date sudo moves
 	std::vector<Bitboard> get_pinning_lines(const Bitboard enemy_king_pos) const;
+	// caclulate once per boardstate
 	void calc_sudo_legal_moves(const Bitboard friends, const Bitboard enemies,
 			bool pawn_move);
+	// caclulate once per boardstate
 	void calc_one_deep_moves(const Bitboard friends, const Bitboard enemies);
+	// caclulate once per boardstate
 	void calc_legal_moves(const Bitboard friends, const Bitboard enemies,
 			Piece *checking, Bitboard checking_line,
 			const std::vector<Bitboard> &pinning, Bitboard enemies_atk);
 	Bitboard get_sudo_legal_moves() const;
 	Bitboard get_legal_moves() const;
+	// caclulate once per boardstate
 	bool checkmate(const Bitboard enemies_atk) const;
+
 	bool move_piece(Move m);
 	bool is_valid_move(Move m);
+
 	void add_piece(Piece p);
 	bool remove_piece(Bitboard pos);
+
 	std::string print_team() const;
 };
 
